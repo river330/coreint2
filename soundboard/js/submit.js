19de644f1b3137017a7f6ae0ad22aba5f556a7e4
river330 <river330@newschool.edu>
2022-05-10 11:38:43 -0400

    making edits for submission and active pages

---
+++ soundboard/js/submit.js
@@ -0,0 +1,9 @@
+console.log("Hello, world!");
+
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: "keyv95oI4fnSU3EgP" }).base(
+  "apprYjkG6aBXj2WbE"
+);
+
+base("Main").select({view: "submitted"}).eachPage(gotPageOfSeasons, gotAllSeasons);
+
